f11b9eda1cc85793d92a5f85ef5aac6d1a94e6d0
Iloking <halylish@gmail.com>
2024-11-16 15:54:10 +0300

    updated

--- main.py
+++ main.py
@@ -3,3 +3,48 @@ print("Bin af a:", bin(a))
 b = ~a
 print("Not:", b)
 print("Bin of b:", bin(b))
+
+a = 10
+b = 6
+print(bin(a))
+print(bin(b))
+print(a & b)
+print(bin(a&b))
+
+a =8
+b = 5
+print(bin(a))
+print(bin(b))
+print(bin a │ b)
+print(bin(a │ b))
+
+a = 10
+b = 8
+print(bin(a))
+print(bin(b))
+print(a ^ b)
+print(bin(a ^ b))
+
+a = 10
+print(a << 1)  # Left shift by 1 multiplies the number by 2
+
+a = 10
+print(a >> 1)  # Right shift by 1 divides the number by 2
+num = 23
+
+if num ^ 1 == (num + 1):
+    print(num, "is even")
+else:
+    print(num, "is odd")
+
+num = 20
+print(bin(num))
+
+counter = 0
+
+while num > 0:
+    counter = counter + 1
+    num = num >> 1
+
+print(counter)
+
